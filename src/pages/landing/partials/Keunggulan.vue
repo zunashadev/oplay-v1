47dbb239496045868ac1336831830b589055dfac
Zun Asha <zunasha080602@gmail.com>
2025-03-12 04:29:42 +0700

    update project

--- src/pages/landing/partials/Keunggulan.vue
+++ src/pages/landing/partials/Keunggulan.vue
@@ -9,28 +9,36 @@
       </p>
       <div class="grid grid-cols-4 gap-6 mx-auto">
         <div class="flex flex-col items-center space-y-6 max-w-56">
-          <div class="bg-firefly-950 size-24 rounded-full"></div>
+          <div class="flex items-center justify-center bg-firefly-950 size-24 rounded-full">
+            <img src="/img/illustrations/low-price.png" class="w-14" />
+          </div>
           <div class="flex flex-col space-y-4 text-center">
             <p class="font-semibold text-xl">Harga Terjangkau</p>
             <p class="font-normal text-base">Lebih murah dari harga resmi</p>
           </div>
         </div>
         <div class="flex flex-col items-center space-y-6 max-w-48">
-          <div class="bg-firefly-950 size-24 rounded-full"></div>
+          <div class="flex items-center justify-center bg-firefly-950 size-24 rounded-full">
+            <img src="/img/illustrations/quick.png" class="w-16 pr-1" />
+          </div>
           <div class="flex flex-col space-y-4 text-center">
             <p class="font-semibold text-xl">Proses Cepat & Otomatis</p>
             <p class="font-normal text-base">Akun dikirim setelah pembayaran</p>
           </div>
         </div>
         <div class="flex flex-col items-center space-y-6 max-w-48">
-          <div class="bg-firefly-950 size-24 rounded-full"></div>
+          <div class="flex items-center justify-center bg-firefly-950 size-24 rounded-full">
+            <img src="/img/illustrations/warranty.png" class="w-14" />
+          </div>
           <div class="flex flex-col space-y-4 text-center">
             <p class="font-semibold text-xl">Garansi Keamanan</p>
             <p class="font-normal text-base">Jika ada masalah, kami siap membantu!</p>
           </div>
         </div>
         <div class="flex flex-col items-center space-y-6 max-w-48">
-          <div class="bg-firefly-950 size-24 rounded-full"></div>
+          <div class="flex items-center justify-center bg-firefly-950 size-24 rounded-full">
+            <img src="/img/illustrations/wallet.png" class="w-12" />
+          </div>
           <div class="flex flex-col space-y-4 text-center">
             <p class="font-semibold text-xl">Beragam Metode Pembayaran</p>
             <p class="font-normal text-base">Transfer bank, e-wallet, dan crypto tersedia</p>
