5aa3ea765b4c4c724ff3de843f49790b3b2314ff
Zun Asha <zunasha080602@gmail.com>
2025-03-13 22:10:10 +0700

    update project

--- src/pages/landing/partials/Keunggulan.vue
+++ src/pages/landing/partials/Keunggulan.vue
@@ -10,41 +10,41 @@
       <div class="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-10 md:grid-cols-4">
         <!--  -->
         <div class="flex items-center space-x-4 sm:max-w-48 sm:flex-col sm:space-y-6 sm:space-x-0">
-          <div class="bg-firefly-950 flex aspect-square size-18 items-center justify-center rounded-full sm:size-24">
-            <img src="/img/illustrations/low-price.png" class="h-auto w-12 sm:w-14" />
+          <div class="bg-firefly-950 flex aspect-square size-16 items-center justify-center rounded-full sm:size-24">
+            <img src="/img/illustrations/low-price.png" class="h-auto w-8 sm:w-14" />
           </div>
           <div class="flex flex-col space-y-0 sm:space-y-4 sm:text-center">
-            <p class="text-lg font-semibold sm:text-xl">Harga Terjangkau</p>
+            <p class="text-md font-semibold sm:text-xl">Harga Terjangkau</p>
             <p class="text-sm font-normal sm:text-base">Lebih murah dari harga resmi</p>
           </div>
         </div>
         <!--  -->
         <div class="flex items-center space-x-4 sm:max-w-48 sm:flex-col sm:space-y-6 sm:space-x-0">
-          <div class="bg-firefly-950 flex aspect-square size-18 items-center justify-center rounded-full sm:size-24">
-            <img src="/img/illustrations/quick.png" class="h-auto w-14 pr-1 sm:w-16" />
+          <div class="bg-firefly-950 flex aspect-square size-16 items-center justify-center rounded-full sm:size-24">
+            <img src="/img/illustrations/quick.png" class="h-auto w-10 pr-1 sm:w-16" />
           </div>
           <div class="flex flex-col space-y-0 sm:space-y-4 sm:text-center">
-            <p class="text-lg font-semibold sm:text-xl">Proses Cepat & Otomatis</p>
+            <p class="text-md font-semibold sm:text-xl">Proses Cepat & Otomatis</p>
             <p class="text-sm font-normal sm:text-base">Akun dikirim setelah pembayaran</p>
           </div>
         </div>
         <!--  -->
         <div class="flex items-center space-x-4 sm:max-w-48 sm:flex-col sm:space-y-6 sm:space-x-0">
-          <div class="bg-firefly-950 flex aspect-square size-18 items-center justify-center rounded-full sm:size-24">
-            <img src="/img/illustrations/warranty.png" class="h-auto w-12 sm:w-14" />
+          <div class="bg-firefly-950 flex aspect-square size-16 items-center justify-center rounded-full sm:size-24">
+            <img src="/img/illustrations/warranty.png" class="h-auto w-8 sm:w-14" />
           </div>
           <div class="flex flex-col space-y-0 sm:space-y-4 sm:text-center">
-            <p class="text-lg font-semibold sm:text-xl">Garansi Keamanan</p>
+            <p class="text-md font-semibold sm:text-xl">Garansi Keamanan</p>
             <p class="text-sm font-normal sm:text-base">Jika ada masalah, kami siap membantu!</p>
           </div>
         </div>
         <!--  -->
         <div class="flex items-center space-x-4 sm:max-w-48 sm:flex-col sm:space-y-6 sm:space-x-0">
-          <div class="bg-firefly-950 flex aspect-square size-18 items-center justify-center rounded-full sm:size-24">
-            <img src="/img/illustrations/wallet.png" class="h-auto w-10 sm:w-12" />
+          <div class="bg-firefly-950 flex aspect-square size-16 items-center justify-center rounded-full sm:size-24">
+            <img src="/img/illustrations/wallet.png" class="h-auto w-8 sm:w-12" />
           </div>
           <div class="flex flex-col space-y-0 sm:space-y-4 sm:text-center">
-            <p class="text-lg font-semibold sm:text-xl">Beragam Metode Pembayaran</p>
+            <p class="text-md font-semibold sm:text-xl">Beragam Metode Pembayaran</p>
             <p class="text-sm font-normal sm:text-base">Transfer bank, e-wallet, dan crypto tersedia</p>
           </div>
         </div>
