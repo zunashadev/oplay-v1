9da5f4f8ca6eab056a0b1e31f88488c8f7fdf9a2
Zun Asha <zunasha080602@gmail.com>
2025-03-16 02:40:38 +0700

    update project

--- src/pages/landing/components/OrderModal.vue
+++ src/pages/landing/components/OrderModal.vue
@@ -41,7 +41,7 @@ watch(
     <div class="flex w-full max-w-7xl bg-black text-white">
       <div class="flex w-7xl flex-col">
         <!-- Start : Header -->
-        <div class="flex justify-between bg-white px-5 py-5 text-black">
+        <div class="flex justify-between bg-white px-8 py-5 text-black">
           <div class="flex items-center divide-x divide-gray-300">
             <div class="pr-4">
               <img :src="`/img/product-logos/${product.logo}`" class="max-h-8 w-auto sm:max-h-10" />
@@ -60,7 +60,7 @@ watch(
         <!-- End : Header -->
 
         <!-- Start : Content -->
-        <div class="flex w-full">
+        <div class="flex h-[calc(100vh-124px)] w-full overflow-auto">
           <!-- Start : Left -->
           <div class="w-full max-w-2/3 space-y-5 px-4 py-5">
             <div class="space-y-3">
@@ -86,22 +86,29 @@ watch(
             <div class="bg-blue-charcoal-950 space-y-3 rounded-lg px-4 py-5">
               <p class="text-lg font-medium">Informasi</p>
               <p class="text-base font-normal">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex eligendi, impedit suscipit doloremque
-                commodi tempora nihil assumenda iusto deleniti expedita quod repellat odio vero facilis. Velit, harum,
-                amet rerum quae autem aut dicta quaerat itaque consequuntur repudiandae repellendus voluptate nihil,
-                fuga facere eveniet impedit saepe accusamus voluptas quam. Ducimus neque similique quasi. Quidem
-                expedita ea, unde nihil sed et inventore consequuntur dignissimos fugit quo, quam dolorem dolores
-                deserunt possimus voluptas quia nulla, corrupti amet sunt architecto suscipit cum debitis voluptates!
-                Voluptatem consequatur adipisci reiciendis. Quaerat repudiandae fuga esse consequatur iure quia
-                consectetur repellendus molestiae, exercitationem facere omnis doloremque rerum optio nam nostrum
-                reiciendis nulla assumenda dolor non ut odio rem aliquid quis vel! Quas, sunt laudantium molestias
-                inventore aliquam ex eius eveniet minus, aspernatur optio fugiat at harum. Repellat fugiat, non
-                consequuntur veritatis iure nihil provident ducimus sunt eius. Iure corrupti nostrum magnam. Eos
-                asperiores dolor nesciunt, ipsa et dolores? Perferendis temporibus, maiores suscipit nulla ipsum
-                blanditiis a, eaque qui repudiandae ad voluptates voluptatibus accusamus minima fuga rem, corrupti
-                architecto atque vel ea neque aliquid sunt? Voluptates blanditiis quisquam libero, iste natus voluptate
-                dolor, sapiente dolore quibusdam debitis aliquam atque illo tenetur iusto quia dolores. Adipisci at
-                aperiam beatae expedita?
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci architecto necessitatibus nam, itaque
+                consequatur, magni quisquam dolorem minus facilis alias rerum eum ipsum repellat nobis animi, quam
+                recusandae. Quo reiciendis quis maxime temporibus ab debitis nemo nostrum a alias aliquid deleniti
+                pariatur molestias, explicabo, sapiente amet repudiandae sit? Quae earum expedita dolores? Animi illum
+                quasi minus beatae in earum iusto tempore, accusamus cum consequatur porro maxime quo fugit. Vero
+                molestiae, natus autem quisquam recusandae qui cupiditate sapiente quasi quae eius voluptatem
+                repellendus eos, pariatur et? Eligendi natus vel illo obcaecati. Ratione totam fugiat non quod
+                perferendis laborum obcaecati delectus sunt. Illo alias odio mollitia quaerat, aliquam et eum voluptatum
+                earum sapiente laboriosam veritatis, ad laudantium repudiandae. Id doloribus quis adipisci in ab veniam
+                incidunt eaque cupiditate minima dolor beatae quam, pariatur commodi fugiat quas molestiae nobis eius
+                distinctio hic? Optio perspiciatis dicta itaque quas dolor dolore, deserunt molestias odit sequi quam
+                excepturi, molestiae possimus dolorem. Reiciendis voluptatum iste consectetur recusandae, voluptate id
+                nobis est? Sapiente minus quod quibusdam, totam accusantium commodi ratione repudiandae. Fuga
+                exercitationem debitis laudantium rem totam labore nobis officiis delectus in. Iure laboriosam
+                praesentium aliquam quam accusamus repudiandae illo quae quidem sit distinctio perspiciatis rem quod
+                consectetur ut corrupti modi optio, corporis, quo laborum ad temporibus ipsa! Aliquam quam modi et,
+                iusto provident ipsa cum veniam sit similique laboriosam minus quae, optio pariatur ex assumenda fugiat
+                cupiditate asperiores, sapiente qui sint quas. Modi, adipisci ipsa dolor placeat provident libero neque
+                commodi non nesciunt corporis ad cum sed quia veniam sapiente, asperiores unde, voluptas expedita optio
+                blanditiis officia corrupti dicta! Repudiandae labore, asperiores vel consequatur, ratione voluptatum
+                adipisci maxime in soluta excepturi numquam, molestias expedita quam obcaecati non iusto error at!
+                Excepturi et dolores aliquam error exercitationem quis expedita dicta similique itaque animi vero soluta
+                eveniet, possimus accusamus.
               </p>
             </div>
           </div>
